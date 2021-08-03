a2c778182ff2b76d7867c1fc9da9e232c0c9d8f6
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-03 13:04:35 +0200

    Init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "<strong>AUTO-SCALING APP AWS</strong><br><br>Small but region proof user!<br>Hello World!"
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
