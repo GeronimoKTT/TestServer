b5f486a0c69d00c497df264a8ee729e861928122
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-04 22:02:28 +0200

    Version 7 Updated scripts

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo docker system prune --all --force
 sudo docker build -t testservername:latest -f /home/ubuntu/Jerry/TestServer/Dockerfile .
 sudo docker run -d -p 3000:3000 testservername:latest
