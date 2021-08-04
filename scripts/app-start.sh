1d087eca8e3bb18398543b7130c7ab026485a646
Dominique <domykanyiktesh01@gmail.com>
2021-08-04 21:44:04 +0200

    Version 6 (#12)
    * Version 5
    
    * Major appspec updates
    
    * Major script update for start.sh
    
    * Updated start.sh

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build . -t testservername:latest --file /home/ubuntu/Jerry/TestServer/Dockerfile
+sudo docker build -t testservername:latest -f /home/ubuntu/Jerry/TestServer/Dockerfile .
 sudo docker run -d -p 3000:3000 testservername:latest
