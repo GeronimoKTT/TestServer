05803a44e8a8ecc69032af4651ec8566ea1272b9
Dominique Kanyik <domykanyiktesh01@gmail.com>
2021-08-04 21:05:26 +0200

    Major appspec updates

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sudo docker container stop $(sudo docker container list -q)
+sudo docker rmi $(sudo docker images -q) --force
+
 sudo docker build -t testservername:latest .
 sudo docker run -d -p 3000:3000 testservername:latest
