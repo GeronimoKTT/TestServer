29f15e36f25529da06c1566c050df1aa9e0cc951
Dominique <domykanyiktesh01@gmail.com>
2021-08-04 21:23:02 +0200

    Dev (#10)
    * Version 5
    
    * Major appspec updates
    
    * Major script update for start.sh

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo docker container stop $(sudo docker container list -q)
-sudo docker rmi $(sudo docker images -q) --force
-
-sudo docker build -t testservername:latest .
+sudo docker build . -t testservername:latest --file /home/ubuntu/Jerry/TestServer/Dockerfile
 sudo docker run -d -p 3000:3000 testservername:latest
